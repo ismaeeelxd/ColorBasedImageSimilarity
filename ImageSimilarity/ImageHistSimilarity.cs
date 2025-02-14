2845f5fa74f98ecb8d02b8da73a376d39c3d71d0
ismaeeelxd <ismailmuhammed2026@gmail.com>
2025-02-14 19:30:04 +0200

    Load imgs happens in 12.8s

--- ImageSimilarity/ImageHistSimilarity.cs
+++ ImageSimilarity/ImageHistSimilarity.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Threading.Tasks;
+
 using static ImageSimilarity.ImageOperations;
 
 namespace ImageSimilarity
@@ -46,11 +49,14 @@ namespace ImageSimilarity
             //TODO: Must somehow calculate the red values, green values, blue values alone AND calculate the median ,std dev.
             /*
              * Have a worker for each channel to calculate the mean, median, std dev in parrallel to reduce time
-             * calculate the prob dist in the same n^2 loop
              */
             RGBPixel [,] imageMatrix = OpenImage(imgPath);
             int height = imageMatrix.GetLength(0);
             int width = imageMatrix.GetLength(1);
+            int redSum = 0, greenSum = 0, blueSum = 0;
+            int redMax = -1, greenMax = -1, blueMax = -1;
+            int redMin = int.MaxValue, greenMin = int.MaxValue, blueMin = int.MaxValue;
+            int totalPixels = height * width;
             ChannelStats redStats = new ChannelStats { Hist = new int[256] };
             ChannelStats greenStats = new ChannelStats { Hist = new int[256] };
             ChannelStats blueStats = new ChannelStats { Hist = new int[256] };
@@ -62,41 +68,95 @@ namespace ImageSimilarity
                 GreenStats = greenStats,
                 RedStats = redStats,
             };
-            int redSum = 0, greenSum = 0, blueSum = 0;
-            int redMax = -1, greenMax = -1, blueMax = -1;
-            int redMin = int.MaxValue, greenMin = int.MinValue, blueMin = int.MaxValue;
+
             for (int i = 0; i < height; ++i)
             {
                 for(int j = 0; j < width; ++j)
                 {
-                    if (imageMatrix[i, j].red > redMax) redMax = imageMatrix[i, j].red;
-                    if (imageMatrix[i, j].blue > blueMax) blueMax = imageMatrix[i, j].blue;
-                    if (imageMatrix[i, j].green > greenMax) greenMax = imageMatrix[i, j].green;
+                    int r = imageMatrix[i, j].red;
+                    int g = imageMatrix[i, j].green;
+                    int b = imageMatrix[i, j].blue;
 
-                    if (imageMatrix[i, j].red < redMin) redMin = imageMatrix[i, j].red;
-                    if (imageMatrix[i, j].green < greenMin) greenMin = imageMatrix[i, j].green;
-                    if (imageMatrix[i, j].blue < blueMin) blueMin = imageMatrix[i, j].blue;
+                    if (r > redMax) redMax = r;
+                    if (g > greenMax) greenMax = g;
+                    if (b > blueMax) blueMax = b;
 
-                    redSum += imageMatrix[i, j].red;
-                    greenSum += imageMatrix[i, j].green;
-                    blueSum += imageMatrix[i, j].blue;
+                    if (r < redMin) redMin = r;
+                    if (g < greenMin) greenMin = g;
+                    if (b < blueMin) blueMin = b;
 
-                    redStats.Hist[imageMatrix[i, j].red]++;
-                    greenStats.Hist[imageMatrix[i, j].green]++;
-                    blueStats.Hist[imageMatrix[i, j].blue]++;
+                    redSum += r;
+                    greenSum += g;
+                    blueSum += b;
+
+                    redStats.Hist[r]++;
+                    greenStats.Hist[g]++;
+                    blueStats.Hist[b]++;
 
                 }
             }
-            throw new NotImplementedException();
+            redStats.Max = redMax; greenStats.Max = greenMax; blueStats.Max = blueMax;
+            redStats.Min = redMin; greenStats.Min = greenMin; blueStats.Min = blueMin;
+            //could potentially have 3 workers here to speed up the process.
+
+            ComputeChannelStats(ref redStats, redSum, totalPixels);
+            ComputeChannelStats(ref greenStats, greenSum, totalPixels);
+            ComputeChannelStats(ref blueStats, blueSum, totalPixels);
+
+            return imageInfo;
         }
+        private static void ComputeChannelStats(ref ChannelStats channelStats,int sum, int totalPixels)
+        {
+            channelStats.Mean = (double)sum / totalPixels;
+
+            
+            int medianPos = (totalPixels + 1) / 2;
+            int cumulativeFreq = 0;
+
+            for (int i = 0; i < 256; ++i)
+            {
+                cumulativeFreq += channelStats.Hist[i];
+                if (cumulativeFreq >= medianPos)    
+                {
+                    channelStats.Med = i;
+                    break;
+                }
+            }
+
+            double sumSquares = 0.0;
+            for (int i = 0; i < 256; i++)
+            {
+                int count = channelStats.Hist[i];
+                if (count == 0) continue;
+                double diff = i - channelStats.Mean;
+                sumSquares += count * diff * diff;
+            }
+            channelStats.StdDev = Math.Sqrt(sumSquares / totalPixels);
+        }
+
+
         /// <summary>
         /// Load all target images and calculate their stats
         /// </summary>
         /// <param name="targetPaths">Path of each target image</param>
         /// <returns>Calculated stats of each target image</returns>
-        public static ImageInfo[] LoadAllImages(string []targetPaths)
+        public static ImageInfo[] LoadAllImages(string[] targetPaths)
         {
-            throw new NotImplementedException();
+            ImageInfo[] imageInfos = new ImageInfo[targetPaths.Length];
+
+            // Create ParallelOptions and set the maximum degree of parallelism
+            var options = new ParallelOptions
+            {
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
+
+            // Use Parallel.For with ParallelOptions
+            Parallel.For(0, targetPaths.Length, options, i =>
+            {
+                imageInfos[i] = CalculateImageStats(targetPaths[i]);
+            });
+
+            return imageInfos;
         }
 
         /// <summary>
@@ -108,7 +168,64 @@ namespace ImageSimilarity
         /// <returns>Top matches (image path & distance score) </returns>
         public static MatchInfo[] FindTopMatches(string queryPath, ImageInfo[] targetImgStats, int numOfTopMatches) 
         {
-            throw new NotImplementedException();
+            ImageInfo queryImg = CalculateImageStats(queryPath);
+            MatchInfo[] matchInfos = new MatchInfo[targetImgStats.Length];
+            //calculate cosine distance here
+            for(int i = 0; i < numOfTopMatches; ++i)
+            {
+                double result = calculateCosineDistance(ref targetImgStats[i], ref queryImg);
+                matchInfos[i] = new MatchInfo
+                {
+                    MatchedImgPath = targetImgStats[i].Path,
+                    MatchScore = result,
+                };
+            }
+            Array.Sort(matchInfos, (p1, p2) => p1.MatchScore.CompareTo(p2.MatchScore));
+
+            return matchInfos.Take(numOfTopMatches).ToArray();
+        }
+
+        private static double calculateCosineDistance(ref ImageInfo imageInfos, ref ImageInfo queryImage)
+        {
+            //qprobdist can be calculated once. instead of everytime FIX THIS 
+            int height = imageInfos.Height;
+            int width = imageInfos.Width;
+            int qheight = queryImage.Height;
+            int qwidth = queryImage.Width;
+            double redDist = 0;
+            double greenDist = 0;
+            double blueDist = 0;
+            double redSum = 0;
+            double normRed1 = 0;
+            double greenSum = 0;
+            double normGreen1 = 0;
+            double blueSum = 0;
+            double normBlue1 = 0;
+            double normRed2 = 0;
+            double normBlue2 = 0;
+            double normGreen2 = 0;
+            for(int i = 0; i < 256; ++i)
+            {
+                double redProbDist = (double)imageInfos.RedStats.Hist[i] / (height * width);
+                double greenProbDist = (double)imageInfos.GreenStats.Hist[i] / (height * width);
+                double blueProbDist = (double)imageInfos.BlueStats.Hist[i] / (height * width);
+                double qredProbDist = (double)queryImage.RedStats.Hist[i] / (qheight * qwidth);
+                double qgreenProbDist = (double)queryImage.GreenStats.Hist[i] / (qheight * qwidth);
+                double qblueProbDist = (double)queryImage.BlueStats.Hist[i] / (qheight * qwidth);
+                redSum += (redProbDist * qredProbDist);
+                greenSum += (greenProbDist * qgreenProbDist);
+                blueSum += (blueProbDist * qblueProbDist);
+                normRed1 += (redProbDist * redProbDist);
+                normBlue1 += (blueProbDist * blueProbDist);
+                normGreen1 += (greenProbDist * greenProbDist);
+                normRed2 += (qredProbDist * qredProbDist);
+                normGreen2 += (qgreenProbDist * qgreenProbDist);
+                normBlue2 += (qblueProbDist * qblueProbDist);
+            }
+            redDist = Math.Acos(redSum / Math.Sqrt(normRed1 * normRed2)) * (180.0 / Math.PI);
+            blueDist = Math.Acos(blueSum / Math.Sqrt(normBlue1 * normBlue2)) * (180.0 / Math.PI);
+            greenDist = Math.Acos(greenSum / Math.Sqrt(normGreen1 * normGreen2)) * (180.0 / Math.PI);
+            return (redDist + blueDist + greenDist) / 3;
         }
     }
 }
